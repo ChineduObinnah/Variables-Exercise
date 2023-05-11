8f9c559bf2e597afebe998ee3264cd38b1c97734
Chinedu Obinnah~ <cobinnah@yahoo.com>
2023-05-11 13:31:27 -0500

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String name = "Mary";
+            int appleNum = 7;
+            char lastIntial = 'O';
+            bool herBool = true;
+            double herhours = 4;
+            decimal applePrice = 2.99m;
+
+            Console.WriteLine($"This is {name} She is selling {appleNum} apples. Her last intial is {lastIntial} and her stan is open for {herhours} " +
+                $"she charges {applePrice} for each apple");
         }
     }
 }
